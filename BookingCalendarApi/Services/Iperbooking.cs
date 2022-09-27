2d642ace424eaf93375e6bdf907f7745c4ae82f9
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-27 22:32:44 +0200

    Implemented get room rates iperbooking service

--- BookingCalendarApi/Services/Iperbooking.cs
+++ BookingCalendarApi/Services/Iperbooking.cs
@@ -1,4 +1,5 @@
 ﻿using BookingCalendarApi.Models.Iperbooking;
+using System.Text.Json;
 
 namespace BookingCalendarApi.Services
 {
@@ -13,7 +14,34 @@ namespace BookingCalendarApi.Services
 
         public async Task<ICollection<Room>> GetRoomRates()
         {
-            throw new NotImplementedException();
+            var url = $"https://api.iperbooking.net/v1/GetRoomRates.cfm?idhotel={auth.IdHotel}&username={auth.Username}&password={auth.Password}&format=json";
+            try
+            {
+                using HttpClient client = new();
+                using HttpResponseMessage response = await client.GetAsync(url);
+                using HttpContent content = response.Content;
+
+                var data = await content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    var poco = JsonSerializer.Deserialize<ICollection<Room>>(data);
+                    if (poco != null)
+                    {
+                        return poco;
+                    } else
+                    {
+                        throw new Exception("Cannot deserialize fetched iperbooking data");
+                    }
+                } else
+                {
+                    throw new Exception("Fetched empty data from iperbooking");
+                }
+            } catch(Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            return new HashSet<Room>();
         }
     }
 }
