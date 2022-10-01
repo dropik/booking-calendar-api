ece359c6ac5167ca2d8f7112392eedf0f2467532
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-01 15:21:24 +0200

    Added db context

---
+++ BookingCalendarApi/BookingCalendarContext.cs
@@ -0,0 +1,12 @@
+﻿using BookingCalendarApi.Models;
+using System.Data.Entity;
+
+namespace BookingCalendarApi
+{
+    public class BookingCalendarContext : DbContext
+    {
+        public DbSet<Floor> Floors { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+    }
+}
