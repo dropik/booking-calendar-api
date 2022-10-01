7008619fcf21e175f2b00dd41204e0d0588fca30
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-01 19:20:21 +0200

    Added first migration

--- BookingCalendarApi/BookingCalendarContext.cs
+++ BookingCalendarApi/BookingCalendarContext.cs
@@ -1,10 +1,12 @@
 ﻿using BookingCalendarApi.Models;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingCalendarApi
 {
     public class BookingCalendarContext : DbContext
     {
+        public BookingCalendarContext(DbContextOptions<BookingCalendarContext> options) : base(options) { }
+
         public DbSet<Floor> Floors { get; set; }
         public DbSet<Room> Rooms { get; set; }
 
