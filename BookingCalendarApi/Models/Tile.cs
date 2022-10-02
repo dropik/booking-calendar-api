4af13f2bc190275d15a07362a93085d937af6870
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-02 19:11:15 +0200

    Added model for api tiles response

---
+++ BookingCalendarApi/Models/Tile.cs
@@ -0,0 +1,16 @@
+namespace BookingCalendarApi.Models
+{
+    public class Tile
+    {
+        public string Id { get; set; }
+        public string BookingId { get; set; }
+        public string Name { get; set; }
+        public string From { get; set; }
+        public uint Nights { get; set; }
+        public string RoomType { get; set; }
+        public string Entity { get; set; }
+        public uint Persons { get; set; }
+        public string Color { get; set; }
+        public long? RoomId { get; set; }
+    }
+}
