59babcba2d49b6a7a0199d861eeeba2d3005628f
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-16 15:20:14 +0200

    Introduced colorized booking model

---
+++ BookingCalendarApi/Services/ColorizedBooking.cs
@@ -0,0 +1,15 @@
+﻿using BookingCalendarApi.Models.Iperbooking.Bookings;
+
+namespace BookingCalendarApi.Services
+{
+    public class ColorizedBooking : Booking
+    {
+        public ColorizedBooking(long bookingNumber, string firstName, string lastName, string lastModified, string color) :
+            base(bookingNumber, firstName, lastName, lastModified)
+        {
+            Color = color;
+        }
+
+        public string Color { get; set; }
+    }
+}
