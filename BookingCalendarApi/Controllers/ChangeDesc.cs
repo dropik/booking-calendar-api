3495c594cf416abc4b2dfcd21d0169e42fccde43
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-16 11:30:38 +0200

    Added changes controller

---
+++ BookingCalendarApi/Controllers/ChangeDesc.cs
@@ -0,0 +1,17 @@
+﻿namespace BookingCalendarApi.Controllers
+{
+    public class ChangeDesc
+    {
+        public ChangeDesc(string from)
+        {
+            From = from;
+        }
+
+        public bool RoomChanged { get; set; } = false;
+        public string From { get; set; }
+        public ulong? OriginalRoom { get; set; }
+        public ulong? NewRoom { get; set; }
+        public string? OriginalColor { get; set; }
+        public string? NewColor { get; set; }
+    }
+}
---
+++ BookingCalendarApi/Controllers/ChangesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingCalendarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChangesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(IDictionary<string, ChangeDesc> changes)
+        {
+            return Ok();
+        }
+    }
+}
