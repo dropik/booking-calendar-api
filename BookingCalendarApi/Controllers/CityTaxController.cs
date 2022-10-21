d266b3c334fd1bb9241e480471e5c4b004601f6d
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-21 14:03:48 +0200

    Added city tax endpoint

--- BookingCalendarApi/Controllers/CityTaxController.cs
+++ BookingCalendarApi/Controllers/CityTaxController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingCalendarApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingCalendarApi.Controllers
 {
     [Route("api/v1/city-tax")]
     public class CityTaxController : ControllerBase
     {
-
+        [HttpGet]
+        public async Task<ActionResult<CityTax>> GetAsync(string from, string to)
+        {
+            return Ok();
+        }
     }
 }
