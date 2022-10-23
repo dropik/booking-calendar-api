e2a4603418622eebd9fb690d21b0d471e60cbd4d
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-10-24 01:02:44 +0200

    Implemented assigned bookings recomposition by only assigned

---
+++ BookingCalendarApi/Services/AssignedBookingComposerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BookingCalendarApi.Models.Iperbooking.Bookings;
+
+namespace BookingCalendarApi.Services
+{
+    public static class AssignedBookingComposerExtensions
+    {
+        public static IEnumerable<AssignedBooking> ExcludeNotAssigned(this IEnumerable<AssignedBooking> bookings) =>
+            bookings
+            .Select(booking => new AssignedBooking(booking.Booking)
+            {
+                Rooms = booking.Rooms
+                    .Where(room => room.RoomId != null)
+            })
+            .Where(booking => booking.Rooms.Any());
+    }
+}
