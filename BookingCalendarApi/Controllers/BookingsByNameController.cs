b55a4aa3e1b2b8416f431b1ba3f95192964f1105
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2022-12-31 02:09:14 +0100

    BC-12: removed booking shorts composer

--- BookingCalendarApi/Controllers/BookingsByNameController.cs
+++ BookingCalendarApi/Controllers/BookingsByNameController.cs
@@ -9,24 +9,42 @@ namespace BookingCalendarApi.Controllers
     public class BookingsByNameController : ControllerBase
     {
         private readonly IIperbooking _iperbooking;
-        private readonly IBookingShortComposer _bookingShortComposer;
+        private readonly BookingCalendarContext _context;
 
-        public BookingsByNameController(IIperbooking iperbooking, IBookingShortComposer bookingShortComposer)
+        public BookingsByNameController(IIperbooking iperbooking, BookingCalendarContext context)
         {
             _iperbooking = iperbooking;
-            _bookingShortComposer = bookingShortComposer;
+            _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ShortBooking>>> GetByNameAsync(string from, string to, string? name)
+        public async Task<ActionResult<List<ShortBooking>>> GetByNameAsync(string from, string to, string? name)
         {
             try
             {
                 var definedName = name ?? "";
-                return (await _iperbooking.GetBookingsAsync(from, to))
+                var bookingsByName = (await _iperbooking.GetBookingsAsync(from, to))
                     .SelectInRange(from, to)
-                    .SelectByName(definedName)
-                    .UseComposer(_bookingShortComposer)
+                    .SelectByName(definedName);
+
+                var bookingsWithColors = from booking in bookingsByName
+                                         join color in _context.ColorAssignments on booking.BookingNumber.ToString() equals color.BookingId into gj
+                                         from color in gj.DefaultIfEmpty()
+                                         select new { Booking = booking, Color = color };
+
+                return bookingsWithColors
+                    .Select(join => new ShortBooking(
+                        id: join.Booking.BookingNumber.ToString(),
+                        name: $"{join.Booking.FirstName} {join.Booking.LastName}",
+                        lastModified: join.Booking.LastModified,
+                        from: DateTime.ParseExact(join.Booking.Rooms.OrderBy(room => room.Arrival).First().Arrival, "yyyyMMdd", null).ToString("yyyy-MM-dd"),
+                        to: DateTime.ParseExact(join.Booking.Rooms.OrderBy(room => room.Departure).Last().Departure, "yyyyMMdd", null).ToString("yyyy-MM-dd"),
+                        occupations: Convert.ToUInt32(join.Booking.Rooms.Count)
+                    )
+                    {
+                        Status = join.Booking.Status,
+                        Color = join.Color?.Color
+                    })
                     .ToList();
             }
             catch (Exception ex)
