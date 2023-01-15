c30dd89c962508e87322e97a835464d9b36a752b
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-01-15 01:09:07 +0100

    Removed exception handling from clients by query controller

---
+++ BookingCalendarApi/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using BookingCalendarApi.Models;
+using BookingCalendarApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingCalendarApi.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientsService _service;
+
+        public ClientsController(IClientsService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("clients-by-query")]
+        public async Task<ActionResult<IEnumerable<ClientWithBooking>>> GetByQuery(string query, string from, string to)
+        {
+            return Ok(await _service.GetByQuery(query, from, to));
+        }
+    }
+}
---
+++ BookingCalendarApi/Services/IClientsService.cs
@@ -0,0 +1,9 @@
+﻿using BookingCalendarApi.Models;
+
+namespace BookingCalendarApi.Services
+{
+    public interface IClientsService
+    {
+        Task<List<ClientWithBooking>> GetByQuery(string query, string from, string to);
+    }
+}
