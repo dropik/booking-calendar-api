aa0f2fc9b8aeea4af8659b84d0a809f8c5bca4c2
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-01-15 21:21:40 +0100

    Sending connection error on istat failed connection

--- BookingCalendarApi/Services/IstatService.cs
+++ BookingCalendarApi/Services/IstatService.cs
@@ -4,6 +4,7 @@ using BookingCalendarApi.Models.Requests;
 using BookingCalendarApi.Models.Responses;
 using C59Service;
 using Microsoft.EntityFrameworkCore;
+using System.ServiceModel;
 
 namespace BookingCalendarApi.Services
 {
@@ -24,66 +25,75 @@ namespace BookingCalendarApi.Services
 
         public async Task<IstatLastDateResponse> GetLastDate()
         {
-            var request = new ultimoC59(_credentials.Username, _credentials.Password, _credentials.Struttura);
-            var response = await _service.ultimoC59Async(request);
-            var lastDate = response.@return.elencoC59
-                .Select(item => item.dataMovimentazione.ToString("yyyy-MM-dd"))
-                .OrderBy(date => date)
-                .First();
-            return new()
+            try
             {
-                LastDate = lastDate,
-            };
+                var request = new ultimoC59(_credentials.Username, _credentials.Password, _credentials.Struttura);
+                var response = await _service.ultimoC59Async(request);
+                var lastDate = response.@return.elencoC59
+                    .Select(item => item.dataMovimentazione.ToString("yyyy-MM-dd"))
+                    .OrderBy(date => date)
+                    .First();
+                return new()
+                {
+                    LastDate = lastDate,
+                };
+            }
+            catch (CommunicationException exception)
+            {
+                throw new BookingCalendarException(BCError.CONNECTION_ERROR, $"Failed establish connection with ISTAT service: {exception.Message}");
+            }
         }
 
         public async Task SendNewData(IstatSendDataRequest request)
         {
-            var to = DateTime.ParseExact(request.Date, "yyyy-MM-dd", null);
-            var lastUploadRequest = new ultimoC59(_credentials.Username, _credentials.Password, _credentials.Struttura);
-            var lastUploadResponse = await _service.ultimoC59Async(lastUploadRequest);
-            var lastUpload = lastUploadResponse.@return.elencoC59
-                .OrderBy(item => item.dataMovimentazione.ToString("yyyy-MM-dd"))
-                .First();
-
-            var nations = await _context.Nations.ToListAsync();
-
-            var fromDate = lastUpload.dataMovimentazione.AddDays(1);
-            if ((to - fromDate).Days < 0)
+            try
             {
-                throw new BookingCalendarException(BCError.ISTAT_ERROR, "Overriding ISTAT history is not possible.");
-            }
+                var to = DateTime.ParseExact(request.Date, "yyyy-MM-dd", null);
+                var lastUploadRequest = new ultimoC59(_credentials.Username, _credentials.Password, _credentials.Struttura);
+                var lastUploadResponse = await _service.ultimoC59Async(lastUploadRequest);
+                var lastUpload = lastUploadResponse.@return.elencoC59
+                    .OrderBy(item => item.dataMovimentazione.ToString("yyyy-MM-dd"))
+                    .First();
 
-            var bookings = await _bookingsProvider.Get(fromDate.ToString("yyyy-MM-dd"), to.ToString("yyyy-MM-dd"), exactPeriod: false);
+                var nations = await _context.Nations.ToListAsync();
 
-            var prevTotal = lastUpload.totalePartenze;
-            var dateCounter = fromDate;
-            while ((to - dateCounter).Days >= 0)
-            {
-                var date = dateCounter.ToString("yyyyMMdd");
-                var arrivedOrDeparturedStays = bookings
-                    .SelectMany(
-                        booking => booking.Rooms
-                            .Where(room => room.Arrival == date || room.Departure == date),
-                        (booking, room) => room
-                    );
-                var guestsWithProvinceOrState = arrivedOrDeparturedStays
-                    .SelectMany(
-                        stay => stay.Guests
-                            .Where(guest =>
-                                guest.BirthCountry != null &&
-                                guest.BirthCountry.Trim() != "" &&
-                                ((guest.BirthCountry != "IT") || (guest.BirthCounty != null && guest.BirthCounty.Trim() != ""))),
-                        (stay, guest) => new
-                        {
-                            stay.Arrival,
-                            stay.Departure,
-                            Country = guest.BirthCountry,
-                            Province = guest.BirthCounty
-                        }
-                    );
-                var movements = guestsWithProvinceOrState
-                    .GroupBy(guest => guest.Country)
-                    .Select(group => group.Key != "IT" ? new List<movimentoWSPO>() {
+                var fromDate = lastUpload.dataMovimentazione.AddDays(1);
+                if ((to - fromDate).Days < 0)
+                {
+                    throw new BookingCalendarException(BCError.ISTAT_ERROR, "Overriding ISTAT history is not possible.");
+                }
+
+                var bookings = await _bookingsProvider.Get(fromDate.ToString("yyyy-MM-dd"), to.ToString("yyyy-MM-dd"), exactPeriod: false);
+
+                var prevTotal = lastUpload.totalePartenze;
+                var dateCounter = fromDate;
+                while ((to - dateCounter).Days >= 0)
+                {
+                    var date = dateCounter.ToString("yyyyMMdd");
+                    var arrivedOrDeparturedStays = bookings
+                        .SelectMany(
+                            booking => booking.Rooms
+                                .Where(room => room.Arrival == date || room.Departure == date),
+                            (booking, room) => room
+                        );
+                    var guestsWithProvinceOrState = arrivedOrDeparturedStays
+                        .SelectMany(
+                            stay => stay.Guests
+                                .Where(guest =>
+                                    guest.BirthCountry != null &&
+                                    guest.BirthCountry.Trim() != "" &&
+                                    ((guest.BirthCountry != "IT") || (guest.BirthCounty != null && guest.BirthCounty.Trim() != ""))),
+                            (stay, guest) => new
+                            {
+                                stay.Arrival,
+                                stay.Departure,
+                                Country = guest.BirthCountry,
+                                Province = guest.BirthCounty
+                            }
+                        );
+                    var movements = guestsWithProvinceOrState
+                        .GroupBy(guest => guest.Country)
+                        .Select(group => group.Key != "IT" ? new List<movimentoWSPO>() {
                         new movimentoWSPO()
                         {
                             italia = false,
@@ -91,38 +101,47 @@ namespace BookingCalendarApi.Services
                             arrivi = group.Where(item => item.Arrival == date).Count(),
                             partenze = group.Where(item => item.Departure == date).Count()
                         }
-                    } : group
-                        .GroupBy(item => item.Province)
-                        .Select(provinceGroup => new movimentoWSPO()
-                        {
-                            italia = true,
-                            targa = provinceGroup.Key,
-                            arrivi = provinceGroup.Where(item => item.Arrival == date).Count(),
-                            partenze = provinceGroup.Where(item => item.Departure == date).Count()
-                        }))
-                    .SelectMany(movements => movements);
+                        } : group
+                            .GroupBy(item => item.Province)
+                            .Select(provinceGroup => new movimentoWSPO()
+                            {
+                                italia = true,
+                                targa = provinceGroup.Key,
+                                arrivi = provinceGroup.Where(item => item.Arrival == date).Count(),
+                                partenze = provinceGroup.Where(item => item.Departure == date).Count()
+                            }))
+                        .SelectMany(movements => movements);
 
-                var totalArrived = movements.Sum(movement => movement.arrivi);
-                var totalDepartured = movements.Sum(movement => movement.partenze);
-                var total = prevTotal + totalArrived - totalDepartured;
+                    var totalArrived = movements.Sum(movement => movement.arrivi);
+                    var totalDepartured = movements.Sum(movement => movement.partenze);
+                    var total = prevTotal + totalArrived - totalDepartured;
 
-                var c59Request = new inviaC59Full(_credentials.Username, _credentials.Password, _credentials.Struttura, new c59WSPO()
-                {
-                    dataMovimentazione = dateCounter,
-                    dataMovimentazioneSpecified = true,
-                    esercizioAperto = true,
-                    totaleArrivi = totalArrived,
-                    totalePartenze = totalDepartured,
-                    totalePresenti = total,
-                    unitaAbitativeDisponibili = 11,
-                    unitaAbitativeOccupate = 0,
-                    movimenti = movements.ToArray()
-                });
+                    var c59Request = new inviaC59Full(_credentials.Username, _credentials.Password, _credentials.Struttura, new c59WSPO()
+                    {
+                        dataMovimentazione = dateCounter,
+                        dataMovimentazioneSpecified = true,
+                        esercizioAperto = true,
+                        totaleArrivi = totalArrived,
+                        totalePartenze = totalDepartured,
+                        totalePresenti = total,
+                        unitaAbitativeDisponibili = 11,
+                        unitaAbitativeOccupate = 0,
+                        movimenti = movements.ToArray()
+                    });
 
-                await _service.inviaC59FullAsync(c59Request);
+                    await _service.inviaC59FullAsync(c59Request);
 
-                prevTotal = total;
-                dateCounter = dateCounter.AddDays(1);
+                    prevTotal = total;
+                    dateCounter = dateCounter.AddDays(1);
+                }
+            }
+            catch (FaultException exception)
+            {
+                throw new BookingCalendarException(BCError.ISTAT_ERROR, $"Error occured while sending data to ISTAT: {exception.Message}");
+            }
+            catch (CommunicationException exception)
+            {
+                throw new BookingCalendarException(BCError.CONNECTION_ERROR, $"Failed establish connection with ISTAT service: {exception.Message}");
             }
         }
     }
