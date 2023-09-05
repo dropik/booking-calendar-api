9411a4ff88f697bffcb6fe56b63de6f2d6d4a826
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-09-05 21:27:26 +0200

    Better clients endpoint names

--- BookingCalendarApi/Controllers/ClientsController.cs
+++ BookingCalendarApi/Controllers/ClientsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookingCalendarApi.Controllers
 {
-    [Route("api/v1")]
+    [Route("api/v1/clients")]
     [ApiController]
     [Authorize]
     public class ClientsController : ControllerBase
@@ -17,13 +17,13 @@ namespace BookingCalendarApi.Controllers
             _service = service;
         }
 
-        [HttpGet("clients-by-query")]
+        [HttpGet("by-query")]
         public async Task<ActionResult<List<ClientWithBookingResponse>>> GetByQuery(string query, string from, string to)
         {
             return Ok(await _service.GetByQuery(query, from, to));
         }
 
-        [HttpGet("clients-by-tile")]
+        [HttpGet("by-tile")]
         public async Task<ActionResult<List<ClientResponse>>> GetByTile(string bookingId, string tileId)
         {
             return Ok(await _service.GetByTile(bookingId, tileId));
