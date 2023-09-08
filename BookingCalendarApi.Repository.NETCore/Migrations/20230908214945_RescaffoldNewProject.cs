4cdf19cbc684e0cbd420ecf630ff172693db25f5
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-09-08 23:50:58 +0200

    Rescaffolding repository

---
+++ BookingCalendarApi.Repository.NETCore/Migrations/20230908214945_RescaffoldNewProject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingCalendarApi.Repository.NETCore.Migrations
+{
+    public partial class RescaffoldNewProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
