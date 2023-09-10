6670828a042400ec04f28bae20fd44e44d05879a
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2023-09-10 18:38:23 +0200

    Added connected services to netframework project

---
+++ BookingCalendarApi.NETFramework/Clients/C59Client.cs
@@ -0,0 +1,35 @@
+﻿using BookingCalendarApi.Models.Clients.C59;
+using BookingCalendarApi.NETFramework.C59Service;
+using BookingCalendarApi.Services;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BookingCalendarApi.Clients
+{
+    public class C59Client : IC59Client
+    {
+        private readonly EC59ServiceEndpoint _service;
+
+        public C59Client(EC59ServiceEndpoint service)
+        {
+            _service = service;
+        }
+
+        private static readonly JsonSerializerOptions _deepCopyOptions = new JsonSerializerOptions()
+        {
+            IncludeFields = true,
+            PropertyNameCaseInsensitive = true,
+        };
+
+        public async Task InviaC59FullAsync(InviaC59FullRequest request)
+        {
+            await _service.inviaC59FullAsync(JsonSerializer.Deserialize<inviaC59Full>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+        }
+
+        public async Task<UltimoC59Response> UltimoC59Async(UltimoC59Request request)
+        {
+            var response = await _service.ultimoC59Async(JsonSerializer.Deserialize<ultimoC59>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<UltimoC59Response>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new UltimoC59Response();
+        }
+    }
+}
---
+++ BookingCalendarApi.NETFramework/Clients/PoliceClient.cs
@@ -0,0 +1,53 @@
+﻿using BookingCalendarApi.Models.Clients.Police;
+using BookingCalendarApi.Services;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BookingCalendarApi.NETFramework.Clients
+{
+    public class PoliceClient : IPoliceClient
+    {
+        private readonly AlloggiatiService.ServiceSoap _service;
+
+        private static readonly JsonSerializerOptions _deepCopyOptions = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            IncludeFields = true,
+        };
+
+        public PoliceClient(AlloggiatiService.ServiceSoap service)
+        {
+            _service = service;
+        }
+
+        public async Task<GenerateTokenResponse> GenerateTokenAsync(GenerateTokenRequest request)
+        {
+            var response = await _service.GenerateTokenAsync(JsonSerializer.Deserialize<AlloggiatiService.GenerateTokenRequest>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<GenerateTokenResponse>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new GenerateTokenResponse();
+        }
+
+        public async Task<RicevutaResponse> RicevutaAsync(RicevutaRequest request)
+        {
+            var response = await _service.RicevutaAsync(JsonSerializer.Deserialize<AlloggiatiService.RicevutaRequest>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<RicevutaResponse>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new RicevutaResponse();
+        }
+
+        public async Task<SendResponse> SendAsync(SendRequest request)
+        {
+            var response = await _service.SendAsync(JsonSerializer.Deserialize<AlloggiatiService.SendRequest>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<SendResponse>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new SendResponse();
+        }
+
+        public async Task<TabellaResponse> TabellaAsync(TabellaRequest request)
+        {
+            var response = await _service.TabellaAsync(JsonSerializer.Deserialize<AlloggiatiService.TabellaRequest>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<TabellaResponse>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new TabellaResponse();
+        }
+
+        public async Task<TestResponse> TestAsync(TestRequest request)
+        {
+            var response = await _service.TestAsync(JsonSerializer.Deserialize<AlloggiatiService.TestRequest>(JsonSerializer.Serialize(request, _deepCopyOptions), _deepCopyOptions));
+            return JsonSerializer.Deserialize<TestResponse>(JsonSerializer.Serialize(response, _deepCopyOptions), _deepCopyOptions) ?? new TestResponse();
+        }
+    }
+}
