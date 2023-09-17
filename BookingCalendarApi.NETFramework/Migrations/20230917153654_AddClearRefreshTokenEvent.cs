2cd5149be5e54c18623c6c24caf520ed2ad02b26
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-09-17 17:50:19 +0200

    Added event trigger to clear refresh tokens

---
+++ BookingCalendarApi.NETFramework/Migrations/20230917153654_AddClearRefreshTokenEvent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingCalendarApi.NETFramework.Migrations
+{
+    public partial class AddClearRefreshTokenEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+create event clear_refresh_tokens
+on schedule every 1 day
+starts current_timestamp
+do
+  delete from userrefreshtokens
+  where ExpiresAt < now();");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop event if exists clear_refresh_tokens");
+        }
+    }
+}
---
+++ BookingCalendarApi.Repository.NETCore/Migrations/20230917154840_AddClearRefreshTokenEvent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingCalendarApi.Repository.NETCore.Migrations
+{
+    public partial class AddClearRefreshTokenEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+create event clear_refresh_tokens
+on schedule every 1 day
+starts current_timestamp
+do
+  delete from userrefreshtokens
+  where ExpiresAt < now();");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop event if exists clear_refresh_tokens");
+        }
+    }
+}
